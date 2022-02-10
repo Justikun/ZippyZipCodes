90bc930c4af298df1d632895530d6463b262f0bc
Justin Lowry <github@lowry.life>
2022-02-09 23:19:37 -0700

    Initial Commit

---
+++ ZippyZipCodes/ZippyZipCodesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZippyZipCodesApp.swift
+//  ZippyZipCodes
+//
+//  Created by Justin Lowry on 2/9/22.
+//
+
+import SwiftUI
+
+@main
+struct ZippyZipCodesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
